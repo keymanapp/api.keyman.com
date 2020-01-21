2c7fcd0d0fdf23ee46719e4a18a98e315f8f85e7
Marc Durdin <marc@keyman.com>
2020-01-21 13:23:18 +1100

    feat: move CJK backend to api.keyman.com
    This translates the CJK backend from SQL Server to
    MySQL, and includes scripts to build the database.

---
+++ script/cjk/chinese_pinyin.php
@@ -0,0 +1,63 @@
+<?php
+  require_once('../../tools/db/db.php');
+  require_once('../../tools/util.php');
+
+  allow_cors();
+  javascript_response();
+
+  if(isset($_GET['py'])) {
+    $py=$_GET["py"]; $id = 0;
+    if(isset($_GET['id'])) $id=$_GET["id"];
+
+    if(($stmt = $mysql->prepare('
+      SELECT pinyin_key, chinese_text, tip FROM cjk.kmw_chinese_pinyin WHERE pinyin_key=? ORDER BY frequency DESC
+    ')) === false) {
+      fail("Failed to prepare query: {$mysql->error}\n");
+    }
+
+    $pylike = $py.'%';
+    $stmt->bind_param("s", $py);
+
+    if($stmt->execute()) {
+      $result = $stmt->get_result();
+      $data = $result->fetch_all();
+
+      $t = ''; $u = '';
+      $tt = '[';
+
+      foreach($data as $row) {
+        $t = $t . $tt . "'" . $row[1] . "'";
+        $u = $u . $tt . "'" . $row[2] . "'";
+        $tt = ',';
+      }
+      if($t == '') $t = '[]'; else $t .= ']';
+      if($u == '') $u = '[]'; else $u .= ']';
+    }
+
+    if(($stmt = $mysql->prepare('
+      SELECT pinyin_key, chinese_text, tip FROM cjk.kmw_chinese_pinyin WHERE pinyin_key LIKE ? AND pinyin_key <> ? ORDER BY frequency DESC LIMIT 20
+    ')) === false) {
+      fail("Failed to prepare query #2: {$mysql->error}\n");
+    }
+
+    $stmt->bind_param("ss", $pylike, $py); //, $idlike, $id);
+    if($stmt->execute()) {
+      $result = $stmt->get_result();
+      $data = $result->fetch_all();
+
+      $t1 = ''; $u1 = '';
+      $tt = '[';
+
+      foreach($data as $row) {
+        $t1 = $t1 . $tt . "'" . $row[1] . "'";
+        $u1 = $u1 . $tt . "'" . $row[2] . "'";
+        $tt = ',';
+      }
+      if($t1 == '') $t1 = '[]'; else $t1 .= ']';
+      if($u1 == '') $u1 = '[]'; else $u1 .= ']';
+    }
+
+    $mysql->close();
+
+    echo "Keyboard_chinese_obj.showCandidates(" . $id . ",'$py'," . $t . "," . $t1 . "," . $u . "," . $u1 . ");";
+  }
---
+++ script/cjk/japanese.php
@@ -0,0 +1,65 @@
+<?php
+  require_once('../../tools/db/db.php');
+  require_once('../../tools/util.php');
+
+  allow_cors();
+  javascript_response();
+
+  if(isset($_GET['kana'])) {
+    $kana=$_GET["kana"]; $id = 0;
+    if(isset($_GET['id'])) $id=$_GET["id"];
+
+    if(($stmt = $mysql->prepare('
+      SELECT DISTINCT kanji, gloss, pri FROM cjk.kmw_japanese WHERE (kana=?) ORDER BY pri
+    ')) === false) {
+      fail("Failed to prepare query: {$mysql->error}\n");
+    }
+
+    $stmt->bind_param("s", $kana);
+    if($stmt->execute()) {
+      $result = $stmt->get_result();
+      $data = $result->fetch_all();
+
+      $t = ''; $u = '';
+      $tt = '[';
+
+      foreach($data as $row) {
+        $kanji = $row[0];
+        $gloss = $row[1];
+        $gloss = str_replace("'","\'",$gloss);
+        $t = $t . $tt . "'" . $kanji . "'";
+        $u = $u . $tt . "'" . $gloss . "'";
+        $tt = ',';
+      }
+      if($t == '') $t = '[]'; else $t .= ']';
+      if($u == '') $u = '[]'; else $u .= ']';
+    }
+
+    if(($stmt = $mysql->prepare('
+      SELECT DISTINCT kanji, gloss, pri FROM cjk.kmw_japanese WHERE ((kana LIKE ?) AND (kana <> ?)) ORDER BY pri LIMIT 20
+    ')) === false) {
+      fail("Failed to prepare query: {$mysql->error}\n");
+    }
+
+    $kanalike = $kana.'%';
+    $stmt->bind_param("ss", $kanalike, $kana);
+    if($stmt->execute()) {
+      $result = $stmt->get_result();
+      $data = $result->fetch_all();
+
+      $t1 = ''; $u1 = '';
+      $tt = '[';
+      foreach($data as $row) {
+        $kanji = $row[0];
+        $gloss = $row[1];
+        $gloss = str_replace("'","\'",$gloss);
+        $t1 = $t1 . $tt . "'" . $kanji . "'";
+        $u1 = $u1 . $tt . "'" . $gloss . "'";
+        $tt = ',';
+      }
+      if($t1 == '') $t1 = '[]'; else $t1 .= ']';
+      if($u1 == '') $u1 = '[]'; else $u1 .= ']';
+    }
+
+    echo "Keyboard_japanese_obj.showCandidates(" . $id . ",'$kana'," . $t . "," . $t1 . "," . $u . "," . $u1 . ");";
+  }
