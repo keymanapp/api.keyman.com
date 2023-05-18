3ba34aefce3662836d9c6971cb77f776ae3106ad
Marc Durdin <marc@durdin.net>
2023-05-18 12:14:32 +0700

    chore: more cleanup

--- build.sh
+++ build.sh
@@ -22,28 +22,25 @@ builder_parse "$@"
 # This script runs from its own folder
 cd "$REPO_ROOT"
 
-declare -A DOCKER_IMAGE DOCKER_CONTAINER
-DOCKER_IMAGE[app]=api-keyman-com-app
-DOCKER_IMAGE[db]=api-keyman-com-db
-DOCKER_CONTAINER[app]=${DOCKER_IMAGE[app]}
-DOCKER_CONTAINER[db]=${DOCKER_IMAGE[db]}
+# We will append -app for app and -db for db
+# Container and image use the same names
+DOCKER_NAME=api-keyman-com
 
 # Get the docker image ID for input parameter
 function _get_docker_image_id() {
-  echo "$(docker images -q ${DOCKER_IMAGE[$1]})"
+  echo "$(docker images -q $DOCKER_NAME-$1)"
 }
 
 # Get the Docker container ID for input parameter
 function _get_docker_container_id() {
-  echo "$(docker ps -a -q --filter ancestor=${DOCKER_CONTAINER[$1]})"
+  echo "$(docker ps -a -q --filter ancestor=$DOCKER_NAME-$1)"
 }
 
 function _stop_docker_container() {
   local API_CONTAINER=$(_get_docker_container_id $1)
-  local CONTAINER_NAME=${DOCKER_CONTAINER[$1]}
 
   if [ ! -z "$API_CONTAINER" ]; then
-    docker container stop ${CONTAINER_NAME}
+    docker container stop $DOCKER_NAME-$1
   else
     builder_echo "No Docker $1 container to stop"
   fi
@@ -61,6 +58,11 @@ function _delete_docker_image() {
   fi
 }
 
+function _get_docker_container_ip() {
+  echo $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $1)
+
+}
+
 builder_run_action configure # no action
 
 # Stop and cleanup Docker containers and images used for the site
@@ -75,13 +77,13 @@ builder_run_action stop:app _stop_docker_container app
 # Build the Docker containers
 if builder_start_action build:db; then
   # Download docker image. --mount option requires BuildKit
-  DOCKER_BUILDKIT=1 docker build -t ${DOCKER_IMAGE[db]} -f mssql.Dockerfile .
+  DOCKER_BUILDKIT=1 docker build -t $DOCKER_NAME-db -f mssql.Dockerfile .
   builder_finish_action success build:db
 fi
 
 if builder_start_action build:app; then
   # Download docker image. --mount option requires BuildKit
-  DOCKER_BUILDKIT=1 docker build -t ${DOCKER_CONTAINER[app]} .
+  DOCKER_BUILDKIT=1 docker build -t $DOCKER_NAME-app .
   builder_finish_action success build:app
 fi
 
@@ -95,8 +97,8 @@ if builder_start_action start:db; then
       -e "ACCEPT_EULA=Y" \
       -e "MSSQL_AGENT_ENABLED=true" \
       -e "MSSQL_SA_PASSWORD=yourStrong(\!)Password" \
-      --name ${DOCKER_IMAGE[db]} \
-      ${DOCKER_CONTAINER[db]}
+      --name $DOCKER_NAME-db \
+      $DOCKER_NAME-db
   else
     builder_echo error "ERROR: Docker database container doesn't exist. Run ./build.sh build first"
     builder_finish_action fail start:db
@@ -120,7 +122,7 @@ if builder_start_action start:app; then
       SITE_HTML="$(pwd):/var/www/html/"
     fi
 
-    db_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${DOCKER_IMAGE[db]})
+    db_ip=$(_get_docker_container_ip $DOCKER_NAME-db)
 
     builder_echo "Spooling up site container"
     docker run --rm -d -p 8098:80 -v ${SITE_HTML} \
@@ -129,8 +131,8 @@ if builder_start_action start:app; then
       -e 'api_keyman_com_mssqlconninfo=sqlsrv:Server='$db_ip',1433;TrustServerCertificate=true;Encrypt=false;Database=' \
       -e api_keyman_com_mssql_create_database=true \
       -e api_keyman_com_mssqldb=keyboards \
-      --name ${DOCKER_IMAGE[app]} \
-      ${DOCKER_CONTAINER[app]}
+      --name $DOCKER_NAME-app \
+      $DOCKER_NAME-app
 
   else
     builder_echo error "ERROR: Docker site container doesn't exist. Run ./build.sh build first"
@@ -144,17 +146,17 @@ if builder_start_action start:app; then
     # TODO: handle vendor/ folder in the way
     # Create link to vendor/ folder
     builder_echo "making link for vendor/ folder"
-    docker exec -i ${DOCKER_IMAGE[app]} sh -c "ln -s /var/www/vendor vendor && chown -R www-data:www-data vendor"
+    docker exec -i $DOCKER_NAME-app sh -c "ln -s /var/www/vendor vendor && chown -R www-data:www-data vendor"
   fi
 
   sleep 15;
   builder_echo "Sleep 15 before attempting to connect to DB"
-  docker exec -i ${DOCKER_IMAGE[app]} sh -c "php /var/www/html/tools/db/build/build_cli.php"
+  docker exec -i $DOCKER_NAME-app sh -c "php /var/www/html/tools/db/build/build_cli.php"
 
   builder_finish_action success start:app
 fi
 
 if builder_start_action test:app; then
-  docker exec -i ${DOCKER_IMAGE[app]} sh -c "php /var/www/html/vendor/bin/phpunit --testdox"
+  docker exec -i $DOCKER_NAME-app sh -c "php /var/www/html/vendor/bin/phpunit --testdox"
   builder_finish_action success test:app
 fi
