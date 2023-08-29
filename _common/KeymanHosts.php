223d03fd0343b96c236c474426cafc4e4835bd20
Darcy Wong <darcy_wong@sil.org>
2023-08-29 15:30:34 +0700

    fix test host for api.keyman.com

--- _common/KeymanHosts.php
+++ _common/KeymanHosts.php
@@ -111,7 +111,7 @@
         $this->r_keymanweb_com = "https://r.keymanweb.com";
       } else if($this->tier == KeymanHosts::TIER_TEST) {
         $this->s_keyman_com = "https://s.keyman.com";
-        $this->api_keyman_com = "https://api.keyman.com";
+        $this->api_keyman_com = "http://host.docker.internal:8098"; // Unique for api.keyman.com
         $this->help_keyman_com = "https://help.keyman.com";
         $this->downloads_keyman_com = "https://downloads.keyman.com";
         $this->keyman_com = "https://keyman.com";
