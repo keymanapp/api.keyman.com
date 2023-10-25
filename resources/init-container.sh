ef3204dd80e88f691fb0762baf9005c0ec68d793
Marc Durdin <marc@durdin.net>
2023-10-25 13:13:49 +1100

    chore: Update init-container.sh

--- resources/init-container.sh
+++ resources/init-container.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Note: currently on TIER_TEST, build.sh skips running this, because the 
-# tests will rebuild the database with static test data anyway. If we add
-# additional steps to this script, we may need to remove this optimization
-# from build.sh.
-
 echo "---- Sleep 15 Before Generating DB ----"
 sleep 15;
-php /var/www/html/tools/db/build/build_cli.php
+
+# If we know we are immediately going to run tests, there's no need to build
+# the database and then rebuild it again as a test database!
+if [[ ! -f /var/www/html/tier.txt ]] || [[ $(</var/www/html/tier.txt) != TIER_TEST ]]; then
+  php /var/www/html/tools/db/build/build_cli.php
+fi
