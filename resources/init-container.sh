62a048bfdcb1efcb9c18197c39b68e8aceb8edd0
Marc Durdin <marc@durdin.net>
2024-02-15 09:05:35 +0700

    fix: force re-download of data when building database
    Fixes #240.

--- resources/init-container.sh
+++ resources/init-container.sh
@@ -6,5 +6,5 @@ sleep 15;
 # If we know we are immediately going to run tests, there's no need to build
 # the database and then rebuild it again as a test database!
 if [[ ! -f /var/www/html/tier.txt ]] || [[ $(</var/www/html/tier.txt) != TIER_TEST ]]; then
-  php /var/www/html/tools/db/build/build_cli.php
+  php /var/www/html/tools/db/build/build_cli.php -f
 fi
